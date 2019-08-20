23b4b2b93b3619e9d3afa18690392c7c56ae4e81
StephenOo <hcqweb_front_end@163.com>
2019-08-20 16:46:57 +0800

    first version

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,9 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <input type="text" v-model="search" @input="searchText()">
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li v-for="(item,index) in results" :key="index" v-html="item"></li>
     </ul>
   </div>
 </template>
@@ -35,6 +13,25 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      search: '',
+      results: ['C·Ronaldo', 'Messi', 'Ramos', 'Modric', 'Marcelo', 'Bale', 'Isco'],
+      copy: ['C·Ronaldo', 'Messi', 'Ramos', 'Modric', 'Marcelo', 'Bale', 'Isco'],
+      update: true
+    }
+  },
+  methods: {
+    searchText() {
+      Object.assign(this.results, this.copy)
+      this.results.forEach((item, index) => {
+        // let replaceReg = new RegExp(this.search, 'g')
+        let replaceString = '<span style="color: #3bbe64;">' + this.search + '</span>'
+        let res = item.replace(this.search, replaceString)
+        this.$set(this.results, index, res)
+      })
+    }
   }
 }
 </script>
@@ -47,12 +44,5 @@ h3 {
 ul {
   list-style-type: none;
   padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
 }
 </style>
